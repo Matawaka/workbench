36dfc5ee535c44515e0a8c7287ce7ea602a1b848
Matawaka <kenkuznecoff@gmail.com>
2026-09-01 23:20:40 +0500

    Route startup to Workbench v0.42

--- src/Matawaka.Workbench.App/App.xaml.cs
+++ src/Matawaka.Workbench.App/App.xaml.cs
@@ -8,7 +8,7 @@ public partial class App : Application
     {
         base.OnStartup(e);
         var window = new MainWindow();
-        window.ConfigureV0412Routing();
+        window.ConfigureV042Routing();
         MainWindow = window;
         window.Show();
     }
