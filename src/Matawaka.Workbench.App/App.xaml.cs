7ea4523cd2e3c6765cd5f4a254e8e36fa2a4fbb0
Matawaka <319695216+Matawaka@users.noreply.github.com>
2026-09-02 18:02:52 +0500

    Activate Workbench v0.49 routing

--- src/Matawaka.Workbench.App/App.xaml.cs
+++ src/Matawaka.Workbench.App/App.xaml.cs
@@ -8,7 +8,7 @@ public partial class App : Application
     {
         base.OnStartup(e);
         var window = new MainWindow();
-        window.ConfigureV048Routing();
+        window.ConfigureV049Routing();
         MainWindow = window;
         window.Show();
     }
