ea86faa44e3797340169bcfd7623062d3cfd75ba
Matawaka <319695216+Matawaka@users.noreply.github.com>
2026-09-02 14:33:30 +0500

    Fold v0.48 suffix helper into package-bound App source

--- src/Matawaka.Workbench.App/V048StringCompatibilityExtensions.cs
+++
@@ -1,7 +0,0 @@
-namespace Matawaka.Workbench.App;
-
-internal static class V048StringCompatibilityExtensions
-{
-    public static bool EndsWith(this string value, char suffix, StringComparison comparisonType)
-        => value.EndsWith(suffix.ToString(), comparisonType);
-}
